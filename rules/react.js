782a8f395d8c861822b14514f6efd97021e58068
Nikolaevskiy Mikhail <mik639inc@gmail.com>
2018-03-11 10:49:14 +0400

    fixes

--- rules/react.js
+++ rules/react.js
@@ -42,7 +42,7 @@ module.exports = {
 
         // Enforce boolean attributes notation in JSX
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
-        'react/jsx-boolean-value': ['error', 'never', { always: [] }],
+        'react/jsx-boolean-value': ['error', 'never'],
 
         // Validate closing bracket location in JSX
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-closing-bracket-location.md
