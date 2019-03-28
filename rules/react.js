02d5c75ddc1396ba5fe046b68fda0b93754a3266
Mikhail Nikolaevskiy <mik639inc@gmail.com>
2019-03-28 16:57:38 +0400

    Resolve conflict between flow and eslint

--- rules/react.js
+++ rules/react.js
@@ -331,7 +331,7 @@ module.exports = {
 
         // Enforce all defaultProps have a corresponding non-required PropType
         // https://github.com/yannickcr/eslint-plugin-react/blob/9e13ae2c51e44872b45cc15bf1ac3a72105bdd0e/docs/rules/default-props-match-prop-types.md
-        'react/default-props-match-prop-types': ['error', { allowRequiredDefaults: false }],
+        'react/default-props-match-prop-types': ['error', { allowRequiredDefaults: true }],
 
         // Prevent usage of shouldComponentUpdate when extending React.PureComponent
         // https://github.com/yannickcr/eslint-plugin-react/blob/9e13ae2c51e44872b45cc15bf1ac3a72105bdd0e/docs/rules/no-redundant-should-component-update.md
